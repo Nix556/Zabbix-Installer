dba1501e783d5cffc216f6f0bc45b85ad7e18e54
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-11-12 18:41:50 +0100

    Update db.sh

--- lib/db.sh
+++ lib/db.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
-
 create_zabbix_db() {
-    local db_name="$1"
-    local db_user="$2"
-    local db_pass="$3"
-    local root_pass="$4"
-
-    info "Creating MariaDB database $db_name and user $db_user..."
-    mysql -uroot -p"$root_pass" <<EOF
-CREATE DATABASE IF NOT EXISTS $db_name CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_pass';
-GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';
-FLUSH PRIVILEGES;
-EOF
-    success "Database $db_name created."
+    local db_name=$1
+    local db_user=$2
+    local db_pass=$3
+    local root_pass=$4
+    sudo mysql -uroot -p"$root_pass" -e "CREATE DATABASE IF NOT EXISTS \`$db_name\` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+    sudo mysql -uroot -p"$root_pass" -e "CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_pass';"
+    sudo mysql -uroot -p"$root_pass" -e "GRANT ALL PRIVILEGES ON \`$db_name\`.* TO '$db_user'@'localhost'; FLUSH PRIVILEGES;"
 }
